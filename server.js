5f13f3c2c52272ec2b6ceb600d21853ed22ea475
bernatixer <bernat.torres.bellido@gmail.com>
2017-03-04 11:11:59 +0100

    Cleaning

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 var express = require('express')
 var app = express()
 
+var Firepad  = require('firepad')
+var firebase = require('firebase')
+
+// Initialize Firebase.
+var config = {
+  apiKey: "AIzaSyCEAf0HUaipyEGIVYL5v_5CatIA9mBGHzQ",
+  authDomain: "team-69132.firebaseapp.com",
+  databaseURL: "https://team-69132.firebaseio.com/"
+}
+firebase.initializeApp(config)
+
+var rootRef = firebase.database().ref()
+var firepadRef = rootRef.push()
+var headless = new Firepad.Headless(firepadRef)
+
+headless.setHtml('<b>Welcome to Firepad!</b>', function(err, committed) {
+  // *err*       will be set if there was a catastrophic failure
+  // *committed* will be true on success, or false if there was a history
+  //               conflict writing to the pad's history.
+});
+
+headless.getText(function(text) {
+  console.log("Contents of firepad retrieved: " + text);
+});
+
 app.get('/', function (req, res) {
   res.send('Hello mon!')
 })
