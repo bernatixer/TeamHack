11a1ba8254098bc6853710cdf50423391a9e9071
arnaubt <arnaubt@gmail.com>
2017-03-04 21:04:36 +0100

    wip on tabs

--- public/tabs.js
+++ public/tabs.js
@@ -19,7 +19,7 @@ $('#create').click(function (e) {
   getNewRef(sugID, function (created, tab) {
     var tabId = tab.key;
     newTab(tab);
-    $(this).closest('li').before('<li class="nav-item" id="' + tabId + '"><a href="#" class="nav-link active" data-toggle="tab">filename</a><span>x</span></li>');
+    $(this).closest('li').before('<li class="nav-item" id="' + tabId + '"><a href="#" class="nav-link active" data-toggle="tab">' + sugID + '</a><span>x</span></li>');
     $('.nav-tabs li:nth-child(' + id + ') a').click();
   });
 });
